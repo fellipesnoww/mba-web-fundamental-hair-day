b7a1a9446dcad54e4e3899bf5e6ca596576ab6ce
fellipesnoww <fellipe2.0e@gmail.com>
2025-04-17 21:49:24 -0300

    config: created webpack, babel, dayjs and json-server config

---
+++ src/libs/dayjs.js
@@ -0,0 +1,5 @@
+import dayjs from "dayjs";
+import 'dayjs/locale/pt-BR';
+
+dayjs.locale('pt-BR');
+
---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict"
+
+//DayJS
+import "./libs/dayjs.js";
+
+//CSS
+import "./styles/form.css";
+import "./styles/global.css";
+import "./styles/schedule.css";
